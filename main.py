f9897eab470481c688e7f21551fedab56923ac40
SunJar99 <beshkempirov.1966@gmail.com>
2025-02-01 00:34:34 +0600

    added base Kitamura

--- main.py
+++ main.py
@@ -31,16 +31,36 @@ class Hero:
 
 
 class ShieldHero(Hero):
-    pass
+    
+    def __init__(self, name, hp, lvl, barrier):
+        super().__init__(name, hp, lvl)
+        self.barrier_1 = barrier
+        
+    def defenseSkill(self):
+        print(f"{self.name_1} uses BarrierUp! {self.barrier_1} of barrier gained!")
+        
+class SpearHero(Hero):
+    
+    def __init__(self, name, hp, lvl, precision):
+        super().__init__(name, hp, lvl)
+        self.precision_1 = precision
+    
+    def spearSkill(self):
+        print(f"{self.name_1} used special skill 'Precision'!, +{self.precision_1}% crit rate gained!")
 
 # Write the difference between ... and pass
 # Both of these functions work almost the same but ... is a bit more complicated than "pass",
 # since "pass" is just for placeholder which doesnt do anything, "..." is an actual expression since it has type of "Ellipsis"
 # For example we can print(...) while we cant print(pass)
 
-naofumi = Hero("NaoFumi", 200, 15)
+naofumi = ShieldHero("NaoFumi", 200, 15, 50)
+kitamura = SpearHero("Kitamura", 115, 16, 20)
 
 naofumi.action()
+kitamura.action()
+
+naofumi.defenseSkill()
+kitamura.spearSkill()
 
 # class --CamelCase
 # object for functions --snek_case
